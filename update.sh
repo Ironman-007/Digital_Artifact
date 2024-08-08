056678dc960e9adefce4cb200644e55f525606ec
fangzheng liu <bitliufangzheng@gmail.com>
2024-08-08 16:30:04 -0400

    UIST demo paper

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 git pull --no-ff
 git status
 git add --all
-git commit -a -m "Encoder seems working now, lower the sample rate to 200Hz"
+git commit -a -m "UIST demo paper"
 git push origin HEAD:main
